8ab256ed710048c4f66eb60bb81cdff4a158ed37
babershere <rehmatq@gmail.com>
2017-12-02 23:02:03 -0800

    created spotify functions

--- keys.js
+++ keys.js
@@ -1,4 +1,4 @@
-console.log('this is loaded');
+// console.log('this is loaded');
 
 var twitterKeys = {
     consumer_key: 'gcvarSWmWSWN1HSBRmiwIBGz4',
--- liri.js
+++ liri.js
@@ -5,27 +5,58 @@ var fs = require('fs');
 var keys = require("./keys.js");
 var command = process.argv[2];
 
-var sob = new Twitter({
+var userSearch = "";
+for (var i = 3; i < process.argv.length; i++) {
+    userSearch += (" "+process.argv[i]);
+}
+
+var twitter = new Twitter({
     consumer_key: keys.twitterKeys.consumer_key,
     consumer_secret: keys.twitterKeys.consumer_secret,
     access_token_key: keys.twitterKeys.access_token_key,
     access_token_secret: keys.twitterKeys.access_token_secret
 });
 
+var spotify = new Spotify({
+    id: keys.spotifyKeys.id,
+    secret: keys.spotifyKeys.secret
+});
+
 function myTweets(){
-console.log(command);
+    twitter.get('statuses/user_timeline/', {count: 20}, function (error, tweets, response) {
+        if (error) throw error;
+        for (var i = 0 ; i < tweets.length; i++) {
+        console.log(tweets[i].text);
+        console.log(tweets[i].created_at);
+        // console.log(tweets.created_at);
+        // console.log(response);  // Raw response object. 
+        }
+    });
+// console.log(command);
+}
+
+function spotty() {
+    spotify
+        .search({ type: 'track', query: userSearch, limit: 1 })
+        .then(function (response) {
+            console.log(response.tracks.items[0].artists[0].name);
+            console.log(response.tracks.items[0].name);
+            console.log(response.tracks.items[0].artists[0].href);
+            console.log(response.tracks.items[0].album.name);
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+
 }
 
 if(command==="my-tweets"){
     myTweets();
 }
 
-function spotify() {
-    console.log("song playing");
-}
 
-if (command === "spotify-this-song") {
-    spotify();
+else if (command === "spotify-this-song") {
+    spotty();
 }
 
 function movie() {
